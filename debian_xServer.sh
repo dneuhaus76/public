fa2530fb4bbf80325264b6ad8598be2eaf358788
Daniel Neuhaus <81403092+dneuhaus76@users.noreply.github.com>
2025-07-16 06:08:52 +0200

    Update debian_xServer.sh

--- debian_xServer.sh
+++ debian_xServer.sh
@@ -242,7 +242,7 @@ fi
 # Stage 2 Chroot
 LANG=$LANG chroot /mnt /bin/bash <<CHROOT_SCRIPT
 # Installiere meine Applikationen - fix für connman
-apt install -yqq xserver-xorg-core lightdm lightdm-settings slick-greeter lxqt xrdp chromium thunderbird libwebkit2gtk-4.0-37
+apt install -yqq xserver-xorg-core lightdm lightdm-settings slick-greeter lxqt lxqt-archiver xrdp chromium thunderbird libwebkit2gtk-4.0-37
 apt install -yqq --no-install-recommends xserver-xorg network-manager-gnome
 apt purge -yqq connman
 apt install -yqq
